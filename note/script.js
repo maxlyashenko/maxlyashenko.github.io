0db91b9b30aeb5d1bfc00f91f9b7c60dc6941b09
maxlyashenko <lyashenko.maks@gmail.com>
2017-10-01 23:54:35 +0300

    note changes

--- note/script.js
+++ note/script.js
@@ -1,3 +1,35 @@
+$('input[name="button"]').on("click",function(){
+       var val = $("#text").val();
+       console.log(val);
+});
+    
+
+var note = [
+    {
+        id: 1,
+        text: "sxsxsxsxs",
+        date: "01.10.2017 22:40"
+    },
+    {
+        id: 2,
+        text: "scscs",
+        date: "01.10.2017 22:45"
+    },    
+    {
+        id: 3,
+        text: "dvvbdvd",
+        date: "01.10.2017 22:50"
+    }
+];
+
+localStorage.setItem("note", JSON.stringify(note));
+var savedNote = JSON.parse(localStorage.getItem("note"));
+for (var i = 0; i < note.length; i++) {
+    document.write(savedNote[i].id + " " + savedNote[i].text + " " + savedNote[i].date);
+}
+
+
+
 $(document).ready(function() {
     $("textarea").hover(function(){
         $("#delete-note").css("display", "block");
